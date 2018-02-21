b389b1908a7438f4388b510329db5494e0f23dce
Larson Carter <larsoncarter@blazermail.net>
2018-02-21 09:20:23 -0600

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  SIMPLESSH
+//
+//  Created by Larson Carter on 2/20/18.
+//  Copyright © 2018 Larson Carter. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hello, There!")
+
+
+print("This Was Developed By Larson Carter!")
+
+
+print("Where Is Your Target?")
+
+
+var target = readLine()
