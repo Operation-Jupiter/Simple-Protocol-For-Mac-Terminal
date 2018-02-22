04026308f0c2a507849cad7f505ceb3ed6a48226
Larson Carter <larsoncarter@blazermail.net>
2018-02-22 07:29:58 -0600

    Update main.swift

--- main.swift
+++ main.swift
@@ -6,38 +6,72 @@
 //  Copyright © 2018 Larson Carter. All rights reserved.
 //
 import Foundation
+
 print("Hello, There!")
+
 print("This Was Developed By Larson Carter!")
+
 print("What Do You Want To Do.")
+
 var command = readLine()
+
 //  This Is Start Of SSH Code.
+
 if (command == "ssh") {
+    
     print("Where Is Your Target? Use IP Address or Domain.")
+    
     let target = readLine()
+    
     if (target == "") {
+        
         print("Error: #1")
+        
     } else {
+        
         print("OK.")
+        
     }
+    
     print("What Is Your Username For Target?")
+    
     let username = readLine()
+    
     print("What Is The Desired Port? Note: Default Is 22! Some Servers Do Not Want A Port!")
+    
     let port = readLine()
+    
     print("ssh " + "\(String(username!))" + "@" + "\(String(target!))" + ":" + "\(String(port!))" )
+    
 } else if (command == "SSH") {
+    
     print("Where Is Your Target? Use IP Address or Domain.")
+    
     let target = readLine()
+    
     if (target == "") {
+        
         print("Error: #1")
+        
     } else {
+        
         print("OK.")
+        
     }
     print("What Is Your Username For Target?")
+    
     let username = readLine()
+    
     print("What Is The Desired Port? Note: Default Is 22! Some Servers Do Not Want A Port!")
+    
     let port = readLine()
+    
     print("ssh " + "\(String(username!))" + "@" + "\(String(target!))" + ":" + "\(String(port!))" )
+    
 } else {
+    
     print("Error: #2")
+    
 }
+
 //  This Is End Of SSH Code.
