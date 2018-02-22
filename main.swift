b136b4e9b00b4be0732e3122290af898d3a00a7e
Larson Carter <larsoncarter@blazermail.net>
2018-02-21 20:37:09 -0600

    Update main.swift
    SSH is completely working. Just need to make things flow better. Going to work on giving to user error codes for better debugging.

--- main.swift
+++ main.swift
@@ -5,38 +5,42 @@
 //  Created by Larson Carter on 2/20/18.
 //  Copyright © 2018 Larson Carter. All rights reserved.
 //
-
 import Foundation
-
 print("Hello, There!")
-
 print("This Was Developed By Larson Carter!")
-
 print("What Do You Want To Do.")
-
-var command1 = readLine()
-
-if ( String(command1!)! == "ssh" || "SSH" ) {
-    
+var command = readLine()
+//  This Is Start Of SSH Code.
+if (command == "ssh") {
     print("Where Is Your Target? Use IP Address or Domain.")
-    
     var target = readLine()
-    
     if (target == "") {
-        
         print("Error: #1")
-        
     } else {
-        
         print("OK.")
-        
     }
-    
+    print("What Is Your Username For Target?")
+    var username = readLine()
+    print("What Is The Desired Port? Note: Default Is 22! Some Servers Do Not Want A Port!")
+    var port = readLine()
+    print("ssh " + "\(String(username!))" + "@" + "\(String(target!))" + ":" + "\(String(port!))" )
+} else if (command == "SSH") {
+    print("Where Is Your Target? Use IP Address or Domain.")
+    var target = readLine()
+    if (target == "") {
+        print("Error: #1")
+    } else {
+        print("OK.")
+    }
+    print("What Is Your Username For Target?")
+    var username = readLine()
+    print("What Is The Desired Port? Note: Default Is 22! Some Servers Do Not Want A Port!")
+    var port = readLine()
+    print("ssh " + "\(String(username!))" + "@" + "\(String(target!))" + ":" + "\(String(port!))" )
 } else {
-    
     print("Error: #2")
-    
 }
+//  This Is End Of SSH Code.
 
 
 
