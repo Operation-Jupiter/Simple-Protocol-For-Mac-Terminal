400eaf86e838462c4f369cc9e0ed71a53f9cefc1
Larson Carter <larsoncarter@blazermail.net>
2018-02-26 16:57:52 -0600

    Create action.swift

---
+++ action.swift
@@ -0,0 +1,19 @@
+//  action.swift
+//  What Protocol To Use
+//  Created by Larson Carter on 2/26/18.
+//  Copyright © 2018 Larson Carter. All rights reserved.
+import Foundation
+print("Hello, There!")
+print("This Was Developed By Larson Carter AKA larsonthekidrs")
+print("What Do You Want To Do?")
+let action = readLine()
+if (action == "ssh" || action == "SSH") {
+    print("Use ssh.swift")
+    print("Open New Terminal Window")
+    print("Type swift")
+    print("Drag ssh.swift into terminal")
+    print("Then you should be using ssh")
+}
+if (action == "telnet" || action == "TELNET") {
+    print("This Is Coming In The Future")
+}
