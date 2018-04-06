c9abed14f9d59ae7f463b88c0f1f7bac493119af
Larson Carter <larsoncarter@blazermail.net>
2018-04-06 11:31:31 -0500

    Telnet, Help!

--- action.swift
+++ action.swift
@@ -1,19 +1,27 @@
-//  action.swift
-//  What Protocol To Use
-//  Created by Larson Carter on 2/26/18.
-//  Copyright © 2018 Larson Carter. All rights reserved.
 import Foundation
 print("Hello, There!")
 print("This Was Developed By Larson Carter AKA larsonthekidrs")
-print("What Do You Want To Do?")
+print("What Protocol Do You Want To Use?")
 let action = readLine()
+if (action == "help" || action == "HELP") {
+    print("Protocols That We Have Working!")
+    print("SSH, TELNET")
+    print("We Have More Coming Soon!")
+    print("Now Exit Out Of This Program")
+}
 if (action == "ssh" || action == "SSH") {
     print("Use ssh.swift")
     print("Open New Terminal Window")
     print("Type swift")
     print("Drag ssh.swift into terminal")
-    print("Then you should be using ssh")
+    print("Then You Should Be Using SSH")
+    print("Now Exit Out Of This Program")
 }
 if (action == "telnet" || action == "TELNET") {
-    print("This Is Coming In The Future")
+    print("Use telnet.swift")
+    print("Open New Terminal Window")
+    print("type swift")
+    print("Drag telnet.swift into terminal")
+    print("Then You Should Be Using Telnet")
+    print("Now Exit Out Of This Program")
 }
